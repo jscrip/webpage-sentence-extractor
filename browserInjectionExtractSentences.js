c6cdec7c7afc76819be1c286e0178336e8afacd4
Jeremy B <jscrip@users.noreply.github.com>
2019-10-16 17:39:39 -0400

    Create browserInjectionExtractSentences.js

---
+++ browserInjectionExtractSentences.js
@@ -0,0 +1,19 @@
+(() => {
+var settings = {
+	minWordCount: 4,
+	minLength: 10
+};
+var sentences = document.querySelector("body")
+	.innerText.replace(/[\n\r\t]+/gim,"[punct]")
+	.replace(/!+\?+|\?+!+/gim,"!?[punct]")
+	.replace(/!+/gim,"![punct]")
+	.replace(/e\.g\.?/gim,"eg")
+	.replace(/\(c\./gim,"(c")
+	.replace(/ c\./gim," c")
+	.replace(/\.+/gim,".[punct]")
+	.replace(/\?+/gim,"?[punct]")
+	.split("[punct]")
+	.filter(str => str.length > settings.minLength && str.split(" ").length >= settings.minWordCount)
+	.map(str => str.trim());
+console.log({sentences});
+})();
